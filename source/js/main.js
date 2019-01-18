31371509380e881c0d4023ec452f60028fb35625
Chizuru Shiomi <chizuru.shiomi@cgi.com>
2019-01-18 13:13:57 -0500

    data

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,13 @@
 var html = '';
 var characterList = document.getElementById('js-character-list');
 
+
+// check if language is jp or en
 characters.forEach(function(character) {
     var characterName = character.name.en.toLowerCase(),
         baseImagePath = 'assets/img/' + characterName + '/';
     html += '<section class="character" id="' + characterName + '"  data-collapsed="true">';
-    html +=     '<h2 class="character-name">' + character.name.jp + '</h2>';
+    html +=     '<h2 class="character-name">' + character.name.en + '</h2>';
     html +=     '<a href="#" class="collapse js-collapse" data-target-element=".character"></a>';
     html +=     '<figure class="portrait"><img src="' + baseImagePath + characterName + '.png" alt=""></figure>';
     html +=     '<div class="skill-trees">';
@@ -20,8 +22,8 @@ characters.forEach(function(character) {
             for (var n = 0; n < character.trees[i].tiers[k].slots.length; n++) {
                 var skill = {
                     slot: character.trees[i].tiers[k].slots[n].slot,
-                    name: character.trees[i].tiers[k].slots[n].name.jp,
-                    description: character.trees[i].tiers[k].slots[n].description.jp
+                    name: character.trees[i].tiers[k].slots[n].name.en,
+                    description: character.trees[i].tiers[k].slots[n].description.en
                 };
                 var imagePath = baseImagePath + 'icons/tree' + (i+1) + '-tier' + (k+1) + '-slot' + skill.slot + '.png';
                 html +=     '<div class="slot slot-' + skill.slot + '">';
@@ -100,8 +102,8 @@ characterList.innerHTML = html;
 // cornelius.skills.forEach(function(skill, index) {
 //     html += '<tr>';
 //     html +=     '<td class="index">' + (index+1) + '</td>';
-//     html +=     '<td>' + skill.name.jp + '</td>';
-//     html +=     '<td>' + skill.description.jp + '</td>';
+//     html +=     '<td>' + skill.name.en + '</td>';
+//     html +=     '<td>' + skill.description.en + '</td>';
 //     html += '</tr>';
 // });
 
