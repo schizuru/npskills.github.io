c37e61b12c47fe674a0acf7ca0b0c88dcba1b28d
Chizuru Shiomi <chizuru.shiomi@cgi.com>
2019-01-18 16:59:39 -0500

    cleaning up

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,6 @@
 var html = '';
 var characterList = document.getElementById('js-character-list');
 
-
-// check if language is jp or en
 characters.forEach(function(character) {
     var characterName = character.name.en.toLowerCase(),
         baseImagePath = 'assets/img/' + characterName + '/';
@@ -12,46 +10,121 @@ characters.forEach(function(character) {
     html +=     '<figure class="portrait"><img src="' + baseImagePath + characterName + '.png" alt=""></figure>';
     html +=     '<div class="skill-trees">';
 
+    // for (var i = 0; i < character.trees.length; i++) {
+    //     html +=     '<div class="tree tree-' + (i+1) + '">';
+    //     for (var k = 0; k < character.trees[i].tiers.length; k++) {
+    //         html +=     '<div class="tier tier-' + (k+1) + '" data-collapsed="true">';
+    //         html +=         '<a href="#" class="collapse js-collapse" data-target-element=".tier"></a>';
+    //         for (var n = 0; n < character.trees[i].tiers[k].slots.length; n++) {
+    //             var skill = {
+    //                 slot: character.trees[i].tiers[k].slots[n].slot,
+    //                 name: character.trees[i].tiers[k].slots[n].name.en,
+    //                 description: character.trees[i].tiers[k].slots[n].description.en
+    //             };
+    //             var imagePath = baseImagePath + 'icons/tree' + (i+1) + '-tier' + (k+1) + '-slot' + skill.slot + '.png';
+    //             html +=     '<div class="slot slot-' + skill.slot + '">';
+    //             html +=         '<figure class="skill-icon">';
+    //             html +=             '<img src="' + imagePath + '" alt="">';
+    //             html +=         '</figure>';
+    //             html +=         '<div class="actions">';
+    //             html +=             '<a href="#" class="button plus">+</a>';
+    //             html +=             '<a href="#" class="button minus">-</a>';
+    //             html +=         '</div>';
+    //             html +=         '<div class="skill-details js-details">';
+    //             html +=             '<h3 class="skill-name">' + skill.name + '</h3>';
+    //             html +=             '<div class="skill-description">' + skill.description + '</div>';
+    //             html +=         '</div>';
+    //             html +=     '</div>';
+    //         }
+    //         html +=     '</div>';
+    //     }
+    //     html +=     '</div>';
+    // }
+    // html +=     '</div>';
+
+    html +=     '<table class="data-table">';
     for (var i = 0; i < character.trees.length; i++) {
-        html +=     '<div class="tree tree-' + (i+1) + '">';
-
         for (var k = 0; k < character.trees[i].tiers.length; k++) {
-            html +=     '<div class="tier tier-' + (k+1) + '" data-collapsed="true">';
-            html +=         '<a href="#" class="collapse js-collapse" data-target-element=".tier"></a>';
-
             for (var n = 0; n < character.trees[i].tiers[k].slots.length; n++) {
                 var skill = {
-                    slot: character.trees[i].tiers[k].slots[n].slot,
-                    name: character.trees[i].tiers[k].slots[n].name.en,
-                    description: character.trees[i].tiers[k].slots[n].description.en
+                    name: {
+                        en: character.trees[i].tiers[k].slots[n].name.en,
+                        jp: character.trees[i].tiers[k].slots[n].name.jp
+                    },
+                    description: {
+                        en: character.trees[i].tiers[k].slots[n].description.en,
+                        jp: character.trees[i].tiers[k].slots[n].description.jp
+                    }
                 };
-                var imagePath = baseImagePath + 'icons/tree' + (i+1) + '-tier' + (k+1) + '-slot' + skill.slot + '.png';
-                html +=     '<div class="slot slot-' + skill.slot + '">';
-                html +=         '<figure class="skill-icon">';
-                html +=             '<img src="' + imagePath + '" alt="">';
-                html +=         '</figure>';
-                html +=         '<div class="actions">';
-                html +=             '<a href="#" class="button plus">+</a>';
-                html +=             '<a href="#" class="button minus">-</a>';
-                html +=         '</div>';
-                html +=         '<div class="skill-details js-details">';
-                html +=             '<h3 class="skill-name">' + skill.name + '</h3>';
-                html +=             '<div class="skill-description">' + skill.description + '</div>';
-                html +=         '</div>';
-                html +=     '</div>';
+                html +=     '<tr>';
+                html +=         '<th>';
+                html +=             '<h3 class="skill-name">' + skill.name.en + '</h3>';
+                html +=             '<h3 class="skill-name">' + skill.name.jp + '</h3>';
+                html +=         '</th>';
+                html +=         '<td>';
+                html +=             '<div class="skill-description">' + skill.description.en + '</div>';
+                html +=             '<div class="skill-description">' + skill.description.jp + '</div>';
+                html +=         '</td>';
+                html +=     '</tr>';
             }
-
-            html +=     '</div>';
         }
-
-        html +=     '</div>';
     }
-
-    html +=     '</div>';
+    html +=     '</table>';
     html += '</section>';
 });
 
 characterList.innerHTML = html;
+
+// check if language is jp or en
+// characters.forEach(function(character) {
+//     var characterName = character.name.en.toLowerCase(),
+//         baseImagePath = 'assets/img/' + characterName + '/';
+//     html += '<section class="character" id="' + characterName + '"  data-collapsed="true">';
+//     html +=     '<h2 class="character-name">' + character.name.en + '</h2>';
+//     html +=     '<a href="#" class="collapse js-collapse" data-target-element=".character"></a>';
+//     html +=     '<figure class="portrait"><img src="' + baseImagePath + characterName + '.png" alt=""></figure>';
+//     html +=     '<div class="skill-trees">';
+
+//     for (var i = 0; i < character.trees.length; i++) {
+//         html +=     '<div class="tree tree-' + (i+1) + '">';
+
+//         for (var k = 0; k < character.trees[i].tiers.length; k++) {
+//             html +=     '<div class="tier tier-' + (k+1) + '" data-collapsed="true">';
+//             html +=         '<a href="#" class="collapse js-collapse" data-target-element=".tier"></a>';
+
+//             for (var n = 0; n < character.trees[i].tiers[k].slots.length; n++) {
+//                 var skill = {
+//                     slot: character.trees[i].tiers[k].slots[n].slot,
+//                     name: character.trees[i].tiers[k].slots[n].name.en,
+//                     description: character.trees[i].tiers[k].slots[n].description.en
+//                 };
+//                 var imagePath = baseImagePath + 'icons/tree' + (i+1) + '-tier' + (k+1) + '-slot' + skill.slot + '.png';
+//                 html +=     '<div class="slot slot-' + skill.slot + '">';
+//                 html +=         '<figure class="skill-icon">';
+//                 html +=             '<img src="' + imagePath + '" alt="">';
+//                 html +=         '</figure>';
+//                 html +=         '<div class="actions">';
+//                 html +=             '<a href="#" class="button plus">+</a>';
+//                 html +=             '<a href="#" class="button minus">-</a>';
+//                 html +=         '</div>';
+//                 html +=         '<div class="skill-details js-details">';
+//                 html +=             '<h3 class="skill-name">' + skill.name + '</h3>';
+//                 html +=             '<div class="skill-description">' + skill.description + '</div>';
+//                 html +=         '</div>';
+//                 html +=     '</div>';
+//             }
+
+//             html +=     '</div>';
+//         }
+
+//         html +=     '</div>';
+//     }
+
+//     html +=     '</div>';
+//     html += '</section>';
+// });
+
+// characterList.innerHTML = html;
 
 
 // var collapseIcons = document.getElementsByClassName('js-collapse');
